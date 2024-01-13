037053406551cd33fa84adb5f2c656d9a8f26c89
João Basto <23194887+joaobasto@users.noreply.github.com>
2024-01-13 21:45:28 +0000

    Change Day 23 to Dynamic Programming

--- Day23/src/main/java/org/example/Main.java
+++ Day23/src/main/java/org/example/Main.java
@@ -19,6 +19,37 @@ public class Main {
         BufferedReader br
                 = new BufferedReader(new FileReader(file));
 
+        //read the nodes into a map
+        Map<Position, Node> nodeByPosition = new HashMap<>();
+        long rowIter = 0;
+        String line;
+        long numberOfColumns = 0;
+        while ((line = br.readLine()) != null) {
+            char[] chars = line.toCharArray();
+            numberOfColumns = chars.length;
+            for (int i = 0; i < chars.length; i++) {
+                if (!(chars[i] == '#')){
+                    nodeByPosition.put(new Position(rowIter, i), new Node(rowIter, i, chars[i]));
+                }
+            }
+            rowIter++;
+        }
+
+        long result = getMaximumDistanceSloped(nodeByPosition.get(new Position(0, 1)), new HashSet<>(), nodeByPosition,
+                nodeByPosition.get(new Position(rowIter - 1, numberOfColumns - 2)));
+        result = result - 1;
+
+        System.out.println("Maximum distance to exit is: " + result);
+    }
+
+    private static void exercise2() throws IOException {
+        System.out.println("Solving Day 23 Challenge 2: ");
+
+        ClassLoader classLoader = Main.class.getClassLoader();
+        File file = new File(classLoader.getResource("input.txt").getFile());
+
+        BufferedReader br
+                = new BufferedReader(new FileReader(file));
 
         //read the nodes into a map
         Map<Position, Node> nodeByPosition = new HashMap<>();
@@ -36,58 +67,143 @@ public class Main {
             rowIter++;
         }
 
-        Position startPosition = new Position(0, 1);
-        Node initialNode = nodeByPosition.get(startPosition);
-        Set<Node> visitedNodes = new HashSet<>();
-        List<Node> toVisitNodes = new ArrayList<>();
-        toVisitNodes.add(initialNode);
+        long result = getMaximumDistance(nodeByPosition.get(new Position(0, 1)), new HashSet<>(), nodeByPosition,
+                nodeByPosition.get(new Position(rowIter - 1, numberOfColumns - 2)));
+        result = result - 1;
 
-        while(!toVisitNodes.isEmpty()) {
-            //take the last node to visit to assure BFS
-            Node node = toVisitNodes.get(toVisitNodes.size() - 1);
-            toVisitNodes.remove(node);
-            //check possible successor nodes and if they were not a predecessor
-            for(Position position : node.getNeighborPositions()) {
-                if (nodeByPosition.get(position) != null && !node.predecessorNodes.contains(nodeByPosition.get(position))
-                && isAccessible(nodeByPosition, position, node)) {
-                    //add them to list of successors and this node to their predecessors
-                    node.successorNodes.add(nodeByPosition.get(position));
-                    nodeByPosition.get(position).predecessorNodes.add(node);
-                    //add them to the toVisitNodes if they were not visited yet
-                    if (!visitedNodes.contains(nodeByPosition.get(position)) &&
-                    !toVisitNodes.contains(nodeByPosition.get(position))) {
-                        toVisitNodes.add(nodeByPosition.get(position));
+        System.out.println("Maximum distance to exit is: " + result);
+    }
+
+
+    public static long getMaximumDistance(Node currentNode, Set<Node> visitedNodes, Map<Position, Node> nodeByPosition, Node finalNode) {
+
+        //get possible new nodes
+        Set<Node> validNeighbors = new HashSet<>();
+        for(Position position : currentNode.getNeighborPositions()) {
+            if (nodeByPosition.get(position) != null && !visitedNodes.contains(nodeByPosition.get(position))) {
+                validNeighbors.add(nodeByPosition.get(position));
+            }
+        }
+
+        //handle terminal case of no validNeighbors
+        if (validNeighbors.isEmpty()) {
+            if(currentNode == finalNode){
+                return 0;
+            } else {
+                return -1000000000;
+            }
+        }
+
+        long tempDistance = 1;
+        Set<Node> newVisitedNodes = new HashSet<>(visitedNodes);
+        newVisitedNodes.add(currentNode);
+        //handle case of only one neighbor
+        if (validNeighbors.size() == 1) {
+            Node newNode = null;
+            while(validNeighbors.size() == 1) {
+                tempDistance++;
+                newNode = validNeighbors.stream().findFirst().get();
+                newVisitedNodes.add(newNode);
+                //get possible new nodes
+                validNeighbors = new HashSet<>();
+                for(Position position : newNode.getNeighborPositions()) {
+                    if (nodeByPosition.get(position) != null && !newVisitedNodes.contains(nodeByPosition.get(position))) {
+                        validNeighbors.add(nodeByPosition.get(position));
                     }
                 }
             }
-            visitedNodes.add(node);
-        }
-
-        visitedNodes.clear();
-        toVisitNodes.clear();
-        toVisitNodes.add(initialNode);
-        while(!toVisitNodes.isEmpty()) {
-            //take the last node
-            Node node = toVisitNodes.get(toVisitNodes.size() - 1);
-            toVisitNodes.remove(node);
-            //update distance and add to visited
-            if (node != initialNode) {
-                node.distance = node.predecessorNodes.stream().map(node2 -> node2.distance).max(Long::compareTo).get() + 1;
-            }
-            visitedNodes.add(node);
-            for(Node sucessorNode : node.successorNodes) {
-                if(visitedNodes.containsAll(sucessorNode.predecessorNodes) && !toVisitNodes.contains(sucessorNode)) {
-                    toVisitNodes.add(sucessorNode);
+            //handle terminal case of no validNeighbors
+            if (validNeighbors.isEmpty()) {
+                if(newNode == finalNode){
+                    return tempDistance;
+                } else {
+                    return -1000000000;
                 }
             }
         }
 
-        Position finalPosition = new Position(rowIter - 1, numberOfColumns - 2);
-        System.out.println("Maximum distance to exit is: " + nodeByPosition.get(finalPosition).distance);
+        long maxDistance = 0;
+        boolean isFirst = true;
+        for(Node neighbor : validNeighbors) {
+            long distance = getMaximumDistance(neighbor, newVisitedNodes, nodeByPosition, finalNode);
+            if(distance > maxDistance || isFirst) {
+                maxDistance = distance;
+            }
+            if (isFirst) {
+                isFirst = false;
+            }
+        }
+        return maxDistance + tempDistance;
     }
 
-    private static boolean isAccessible(Map<Position, Node> nodeByPosition, Position position, Node node) {
+    public static long getMaximumDistanceSloped(Node currentNode, Set<Node> visitedNodes, Map<Position, Node> nodeByPosition, Node finalNode) {
+
+        //get possible new nodes
+        Set<Node> validNeighbors = new HashSet<>();
+        for(Position position : currentNode.getNeighborPositions()) {
+            if (nodeByPosition.get(position) != null && !visitedNodes.contains(nodeByPosition.get(position))
+            && isAccessibleSlope(nodeByPosition, position, currentNode)) {
+                validNeighbors.add(nodeByPosition.get(position));
+            }
+        }
+
+        //handle terminal case of no validNeighbors
+        if (validNeighbors.isEmpty()) {
+            if(currentNode == finalNode){
+                return 0;
+            } else {
+                return -1000000000;
+            }
+        }
+
+        long tempDistance = 1;
+        Set<Node> newVisitedNodes = new HashSet<>(visitedNodes);
+        newVisitedNodes.add(currentNode);
+        //handle case of only one neighbor
+        if (validNeighbors.size() == 1) {
+            Node newNode = null;
+            while(validNeighbors.size() == 1) {
+                tempDistance++;
+                newNode = validNeighbors.stream().findFirst().get();
+                newVisitedNodes.add(newNode);
+                //get possible new nodes
+                validNeighbors = new HashSet<>();
+                for(Position position : newNode.getNeighborPositions()) {
+                    if (nodeByPosition.get(position) != null && !newVisitedNodes.contains(nodeByPosition.get(position))
+                            && isAccessibleSlope(nodeByPosition, position, newNode)) {
+                        validNeighbors.add(nodeByPosition.get(position));
+                    }
+                }
+            }
+            //handle terminal case of no validNeighbors
+            if (validNeighbors.isEmpty()) {
+                if(newNode == finalNode){
+                    return tempDistance;
+                } else {
+                    return -1000000000;
+                }
+            }
+        }
+
+        long maxDistance = 0;
+        boolean isFirst = true;
+        for(Node neighbor : validNeighbors) {
+            long distance = getMaximumDistanceSloped(neighbor, newVisitedNodes, nodeByPosition, finalNode);
+            if(distance > maxDistance || isFirst) {
+                maxDistance = distance;
+            }
+            if (isFirst) {
+                isFirst = false;
+            }
+        }
+        return maxDistance + tempDistance;
+    }
+
+    private static boolean isAccessibleSlope(Map<Position, Node> nodeByPosition, Position position, Node node) {
         Node destinationNode = nodeByPosition.get(position);
+        if(node.type == '.') {
+            return true;
+        }
         if(node.type == '>' && destinationNode.x == node.x && destinationNode.y == (node.y + 1)) {
             return true;
         }
@@ -97,29 +213,6 @@ public class Main {
         if(node.type == 'v' && destinationNode.x == (node.x + 1) && destinationNode.y == node.y) {
             return true;
         }
-        if(destinationNode.type == '>' && destinationNode.x == node.x && destinationNode.y == (node.y - 1)) {
-            return false;
-        }
-        if(destinationNode.type == '<' && destinationNode.x == node.x && destinationNode.y == (node.y + 1)) {
-            return false;
-        }
-        if(destinationNode.type == 'v' && destinationNode.x == (node.x - 1) && destinationNode.y == node.y) {
-            return false;
-        }
-        return true;
-    }
-
-    private static void exercise2() throws IOException {
-        System.out.println("Solving Day 23 Challenge 2: ");
-
-        ClassLoader classLoader = Main.class.getClassLoader();
-        File file = new File(classLoader.getResource("input.txt").getFile());
-
-        BufferedReader br
-                = new BufferedReader(new FileReader(file));
-
-        String line;
-        while ((line = br.readLine()) != null) {
-        }
+        return false;
     }
 }
